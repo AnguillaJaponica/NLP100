8efc5b4158df02165b6313b94ee307bfaf590864
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-04-06 21:03:37 +0900

    chap3_21

--- chapter3/20.py
+++ chapter3/20.py
@@ -7,5 +7,5 @@ def wiki_reader(title, json_data):
         for line in f:
             obj = json.loads(line)
             if obj['title'].encode('utf-8') == title:
-                return obj['text']
+                print(obj['text'])
 wiki_reader('イギリス', 'data/jawiki-country.json.gz')
